2975a29e9606327ff3b940bb0a9c64d1c4963bfe
Kunal Pal <124446202+kunalpal97@users.noreply.github.com>
2024-08-20 13:14:43 +0000

    Operation on variables

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,17 +1,17 @@
 
-let  marks = "kunal"
+// let  marks = "kunal"
 
 
-console.log(typeof marks);
-console.log(typeof(marks));
+// console.log(typeof marks);
+// console.log(typeof(marks));
 
 
 
-let checkValue = Number(marks)
+// let checkValue = Number(marks)
 
-console.log(typeof(checkValue));
+// console.log(typeof(checkValue));
 
-console.log(checkValue);
+// console.log(checkValue);
 
 
 // data type pata karna hai 
@@ -24,24 +24,79 @@ console.log(checkValue);
 //
 
 
-let age = null
+// let age = null
 
 
-console.log(typeof(age));
+// console.log(typeof(age));
 
-let ConvertString = String(age);
+// let ConvertString = String(age);
 
-console.log(typeof(ConvertString));
+// console.log(typeof(ConvertString));
 
 
-let isLoggedIn = "";
+// let isLoggedIn = "";
 
-let booelanIsloggedIn = Boolean(isLoggedIn);
+// let booelanIsloggedIn = Boolean(isLoggedIn);
 
-console.log(booelanIsloggedIn);
+// console.log(booelanIsloggedIn);
 
 // " " -> bool val is false
 // "kunal" -> bool val is true
+
+/*<---------------------------- Operations ---------------------> */
+
+// basic opertaion on Numbers and Strings
+
+// let name = "Kunal";
+// let age  = 22;
+
+// console.log(name + " " + age);
+
+// console.log(23%12);
+
+// console.log(5**3);
+
+// let str1 = "Hello";
+
+// console.log(str1);
+
+// str1 = "test";
+
+// console.log(str1);
+
+let str1 = "Kunal";
+
+let str2 = " Pal";
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1" + 2 + 3 + 4); // value as a string add ho rahi hai yaha pe
+console.log("1" + "2"); //  same here values are adding as a string here
+console.log(1 + 2);  // here both are the numeric values so it gets added here 
+
+
+console.log( 1 + 2 + 6 + "5");  // here first the values get added and then it will give the result 
+
+console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 5;
+
+let GameCounter = 100;
+
+++GameCounter;
+
+console.log(GameCounter);
+
+
+
+
+
+
+
 
 
 
