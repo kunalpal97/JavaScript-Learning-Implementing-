638810edd124c2a5b5f6ccd7e688880e43dccde4
Kunal Pal <124446202+kunalpal97@users.noreply.github.com>
2024-08-20 13:34:49 +0000

    Comparision of datatype

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,30 @@
+
+// console.log(5>1);
+
+// console.log(5 != 5);
+
+// console.log(5 == "5");
+
+// console.log("5" < 6);
+
+// even in typescript is not allowing to compare the two different data type
+
+
+// console.log(null >= 0);
+// console.log(null  == 0);
+// console.log(null <= 0);
+
+// console.log(undefined === 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+// === strictly check is used to compare not only the values but its also check the data type 
+
+console.log("5" !== 5)
+
+
+
+
+
+
+
